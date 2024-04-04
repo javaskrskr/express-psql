16350d42e8f967a4a223c99a5992d2fdbfd60b70
javaskrskr <monkeichow2@gmail.com>
2024-04-04 14:23:47 +0900

    db folder cleanup

--- db/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE schools( 
-    id SERIAL PRIMARY KEY, 
-    name VARCHAR(100), 
-    address VARCHAR(100),
-    created_at INTEGER DEFAULT EXTRACT(EPOCH FROM NOW())
-);
-
-INSERT INTO schools (name, address) VALUES ('John', 'Tokyo');
